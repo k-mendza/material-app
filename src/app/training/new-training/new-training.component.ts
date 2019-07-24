8f3f1673a47f1361eb7628eba68a652602144668
k-mendza <karolmendza@gmail.com>
2019-07-24 11:30:44 +0200

    exercise data routing change

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { TrainingService } from '../training.service';
+import {Exercise} from "../exercise.model";
 
 @Component({
   selector: 'app-new-training',
@@ -7,10 +9,11 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class NewTrainingComponent implements OnInit {
   @Output() trainingStart = new EventEmitter<void>();
-
-  constructor() { }
+  exercises: Exercise[] = []
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
+    this.exercises = this.trainingService.getAvailableExercises();
   }
 
   onStart() {
